b795a2bd78be1f0d6cfd591adf6da9733b7c7cb5
AnhNguyenGit <anhnd1503@gmail.com>
2016-01-22 11:54:20 +0700

    imageUpload control create

--- js/gonrin.uploadfile.js
+++ js/gonrin.uploadfile.js
@@ -3,9 +3,9 @@
     $.fn.extend({
     	fileUpload: function(options){
     		var defaults = {
-    			padding: 20,
-    			template: '<div class="btn btn-file inputbutton btn-info btn-fileupload-medium" > <span class="fileupload-exists" id="btnBrowserFile">Select File Upload...</span> <input type="file" id="selectfile" name="file" /></div>' ,
-            	Element: this,
+                background: 'gray',    			
+                itemTemplate: '<li class="list-group-item item" style="float:left; font-size:15px;"> <p> Itme 1 </p><img class="btn-item" src="images/delete.png" ></img></li>',
+                Element: this,
                 files: [], 
                 Url:'',
                 method:'',
@@ -14,9 +14,9 @@
     		};
 
             var settings = $.extend(defaults, options);
-            var itemTemplate = '<div class="item"> <p class="item-content" style="float:left; font-size:15px;"> Itme 1 </p> <img class="btn-item" src="images/delete.png" style="width:15px; hight:15px; "> </div>';
+            var template = '<div class="container" id="upload-container"></div>';
 
-            
+            var Element = defaults.Element;
             var files = defaults.files;
 
             var filelist={
@@ -64,6 +64,48 @@
                 };
                 xhttp.send(fd);
 
+                },
+
+                init:function(){
+                    console.log(Element);
+                    Element.append(template);
+                    header = $('#upload-container');
+                    header.append('<div id="upload-header"></div>');
+                    header = $('#upload-header');
+                    header.append(' <input type="text" class="form-control" style="  float:left; width:50%" value="No File Selected.." readonly>');
+                    header.append('  <img src="images/up.png" id="btn-view-list" class="btn-upload-control " title="view list"></img>');
+                    header.append('<img  class="btn-upload-control" id="btnBrowserFile" src="images/browserfile.png" title="browser file upload"></img>');
+                    header.append(' <img src="images/upload.png" class="btn-upload-control  " title="upload all"></img>');
+                    header.append(' <img src="images/delete.png" class=" btn-upload-control " title="delete"></img>');
+                    header.append('<div class="btn " ><input type="file" id="selectfile" style="display:none;"   name="file" multiple /></div>');
+                    Element.append('<ul class="list-group" id="list-group">  </ul>');
+                    var list = $('#list-group');
+                    console.log(defaults.itemTemplate);
+                    list.append(defaults.itemTemplate);
+
+                     $('#btn-view-list').click(function(){
+           
+            if($('.list-group').css('display')=='none')
+               {  $('.list-group').show('slow');
+                    $(this).attr('src','images/down.png');
+       }
+             else if($('.list-group').css('display')=='block')            
+                {
+                    $('.list-group').hide('slow');
+                     $(this).attr('src','images/up.png');
+                 }
+        });
+
+            
+        $('#btnBrowserFile').click(function(){
+            $('#selectfile').click();
+        });
+           $('#selectfile').change(function(){
+
+            var numberfile = $(this)[0].files.length;
+            $('input[type=text]').val(numberfile + " files selected");
+
+           });
                 }                
                 
                 
@@ -75,9 +117,10 @@
 
            
             // Khoi tao giao dien ban dau
+            methods.init();
            
-            $('.container').html(defaults.template);
-            $('.container').after('<input type="button" id="btn-upload" value="Upload" class="btn-primary">');
+          //  $('.container').html(defaults.template);
+           // $('.container').after('<input type="button" id="btn-upload" value="Upload" class="btn-primary">');
             
 
             //Bat su kien Select file

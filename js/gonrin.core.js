26838693cc9c2d639cfa32395da4ff55779c4fe1
CuongNC <cuongnc@gonrin.com>
2016-07-24 18:58:13 +0700

    add serializeFormJSON to core

--- js/gonrin.core.js
+++ js/gonrin.core.js
@@ -205,6 +205,24 @@
 	
 		  return template;
   	};
+  	
+  	grexports.serializeFormJSON = function ($el) {
+  		//
+  		//if($el)
+        var o = {};
+        var a = $el.serializeArray();
+        $.each(a, function () {
+            if (o[this.name]) {
+                if (!o[this.name].push) {
+                    o[this.name] = [o[this.name]];
+                }
+                o[this.name].push(this.value || '');
+            } else {
+                o[this.name] = this.value || '';
+            }
+        });
+        return o;
+    };
   
   	grexports.init = function(_$) {
   		return init(_$ || $);
